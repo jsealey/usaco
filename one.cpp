7d6ec54e8072e4353a503ba764ff3cd730a3d83b
Jared Sealey <jsealey762@gmail.com>
2012-10-09 17:00:44 -0700

    Section 1.4 halfway done

--- one.cpp
+++
@@ -1,7 +0,0 @@
-    stdout.put(nl,"###\******VENDING MACHINE******/###",nl);
-    stdout.put("Item 1: " , itemOne.itemName, " | Cost: " , itemOne.cost, " | stock[" , itemOne.amount, "]",nl);
-    stdout.put("Item 2: " , itemTwo.itemName, " | Cost: " , itemTwo.cost, " | stock[" , itemTwo.amount, "]",nl);
-    stdout.put("Item 3: " , itemThree.itemName, " | Cost: " , itemThree.cost, " | stock[" , itemThree.amount, "]",nl);
-    stdout.put("Item 4: " , itemFour.itemName, " | Cost: " , itemFour.cost, " | stock[" , itemFour.amount, "]", nl);
-    stdout.put("Item 5: " , itemFive.itemName, " | Cost: " , itemFive.cost, " | stock[" , itemFive.amount, "]",nl);
-    stdout.put("Item 6: " , itemSix.itemName, " | Cost: " , itemSix.cost, " | stock[" , itemSix.amount, "]",nl);
--- two.cpp
+++
@@ -1,8 +0,0 @@
-    stdout.put(nl, "---******VENDING MACHINE******---" ,nl);
-    stdout.put("Item 1: " , itemOne.itemName, " | Cost: " , itemOne.cost, " | stock[" , itemOne.stock, "]",nl);
-    stdout.put("Item 2: " , itemTwo.itemName, " | Cost: " , itemTwo.cost, " | stock[" , itemTwo.stock, "]",nl);
-    stdout.put("Item 3: " , itemThree.itemName, " | Cost: " , itemThree.cost, " | stock[" , itemThree.stock, "]",nl);
-    stdout.put("Item 4: " , itemFour.itemName, " | Cost: " , itemFour.cost, " | stock[" , itemFour.stock, "]", nl);
-    stdout.put("Item 5: " , itemFive.itemName, " | Cost: " , itemFive.cost, " | stock[" , itemFive.stock, "]",nl);
-    stdout.put("Item 6: " , itemSix.itemName, " | Cost: " , itemSix.cost, " | stock[" , itemSix.stock, "]",nl);
-    stdout.put("Hit enter to continue...");
